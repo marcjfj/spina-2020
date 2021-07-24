2856e393dd58049d85637951b619504260f82f42
Marc <marcjfj@gmail.com>
2021-07-24 17:24:43 -0500

    less stuff

--- src/pages/[page].tsx
+++ src/pages/[page].tsx
@@ -13,74 +13,74 @@ import InstagramEmbed from "react-instagram-embed";
 import YouTube from "react-youtube";
 import { TwitterTweetEmbed } from "react-twitter-embed";
 
-export type Props = {
-  title: string;
-  dateString: string;
-  slug: string;
-  tags: string[];
-  author: string;
-  description?: string;
-  source: MdxRemote.Source;
-};
+// export type Props = {
+//   title: string;
+//   dateString: string;
+//   slug: string;
+//   tags: string[];
+//   author: string;
+//   description?: string;
+//   source: MdxRemote.Source;
+// };
 
-const components = { InstagramEmbed, YouTube, TwitterTweetEmbed };
-const slugToPostContent = (postContents => {
-  let hash = {}
-  postContents.forEach(it => hash[it.slug] = it)
-  return hash;
-})(fetchPostContent());
+// const components = { InstagramEmbed, YouTube, TwitterTweetEmbed };
+// const slugToPostContent = (postContents => {
+//   let hash = {}
+//   postContents.forEach(it => hash[it.slug] = it)
+//   return hash;
+// })(fetchPostContent());
 
-export default function Post({
-  title,
-  dateString,
-  slug,
-  tags,
-  author,
-  description = "",
-  source,
-}: Props) {
-  const content = hydrate(source, { components })
-  return (
-    <PostLayout
-      title={title}
-      date={parseISO(dateString)}
-      slug={slug}
-      tags={tags}
-      author={author}
-      description={description}
-    >
-      {content}
-    </PostLayout>
-  )
-}
+// export default function Post({
+//   title,
+//   dateString,
+//   slug,
+//   tags,
+//   author,
+//   description = "",
+//   source,
+// }: Props) {
+//   const content = hydrate(source, { components })
+//   return (
+//     <PostLayout
+//       title={title}
+//       date={parseISO(dateString)}
+//       slug={slug}
+//       tags={tags}
+//       author={author}
+//       description={description}
+//     >
+//       {content}
+//     </PostLayout>
+//   )
+// }
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  const paths = fetchPostContent().map(it => "/" + it.slug);
-  return {
-    paths,
-    fallback: false,
-  };
-};
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   const paths = fetchPostContent().map(it => "/" + it.slug);
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// };
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const slug = params.post as string;
-  const source = fs.readFileSync(slugToPostContent[slug].fullPath, "utf8");
-  const { content, data } = matter(source, {
-    engines: { yaml: (s) => yaml.load(s, { schema: yaml.JSON_SCHEMA }) as object }
-  });
+// export const getStaticProps: GetStaticProps = async ({ params }) => {
+//   const slug = params.post as string;
+//   const source = fs.readFileSync(slugToPostContent[slug].fullPath, "utf8");
+//   const { content, data } = matter(source, {
+//     engines: { yaml: (s) => yaml.load(s, { schema: yaml.JSON_SCHEMA }) as object }
+//   });
 
-  const mdxSource = await renderToString(content, { components, scope: data });
+//   const mdxSource = await renderToString(content, { components, scope: data });
   
-  return {
-    props: {
-      title: data.title,
-      dateString: data.date,
-      slug: data.slug,
-      description: "",
-      tags: data.tags,
-      author: data.author,
-      source: mdxSource
-    },
-  };
-};
+//   return {
+//     props: {
+//       title: data.title,
+//       dateString: data.date,
+//       slug: data.slug,
+//       description: "",
+//       tags: data.tags,
+//       author: data.author,
+//       source: mdxSource
+//     },
+//   };
+// };
 

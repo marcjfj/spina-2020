41473c4365a16207086bff7037c6c47203dac153
Marc <marcjfj@gmail.com>
2021-08-08 20:26:32 -0500

    added schema

--- public/admin/config/homePage.js
+++ public/admin/config/homePage.js
@@ -3,21 +3,25 @@ import Blocks from './blocks/index.js';
 export default {
   name: "HomePage",
   label: "Home Page",
-  file: "content/homepage/index.mdx",
-  extension: "mdx",
-  format: "frontmatter",
-  fields: [
-    {
-      label: "Slug",
-      name: "slug",
-      widget: "string",
-      required: false,
-    },
-    {
-      label: "Title",
-      name: "title",
-      widget: "string",
-    },
-    {...Blocks}
-  ],
+  files: {
+    name: "general",
+    label: "Settings",
+    file: "content/homepage/index.mdx",
+    extension: "mdx",
+    format: "frontmatter",
+    fields: [
+      {
+        label: "Slug",
+        name: "slug",
+        widget: "string",
+        required: false,
+      },
+      {
+        label: "Title",
+        name: "title",
+        widget: "string",
+      },
+      {...Blocks}
+    ],
+  }
 }

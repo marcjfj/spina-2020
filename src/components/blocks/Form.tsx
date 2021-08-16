a9e5b030b5d13c308f36240941ba7a4232415e13
Marc <marcjfj@gmail.com>
2021-08-15 22:16:27 -0500

    form good

--- src/components/blocks/Form.tsx
+++ src/components/blocks/Form.tsx
@@ -1,28 +1,63 @@
+import {useRouter} from 'next/router';
+import { useRef, useState } from 'react';
 export default function Form({inputs}) {
   console.log(inputs)
+  const formRef = useRef(null);
+  const [formState, setFormState] = useState({...inputs.reduce((acc, input) => {
+    return {...acc, [input.Label]: ""}
+  }, {})});
+  const [formSubmitted, setFormSubmitted] = useState(false);
+  const router = useRouter();
+  const successMsg = () => {
+    return formSubmitted ? (
+        <div className="success-msg">
+          Submitted! 🎉
+        </div>
+      ) : null
+  }
+  function encode(data) {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&")
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let formData = new FormData(formRef.current);
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": formRef.current.getAttribute("name"),
+        ...formState
+      })
+    }).then(() => setFormSubmitted(true)).catch(error => alert(error))
+}
   return (
     <section className="form-wrapper">
       <form 
+        ref={formRef}
         method="POST" 
         data-netlify="true" 
         name="contact" 
-        action="/contact"
+        action="/contact?submitted=true"
         className="form">
+        {successMsg()}
         {inputs.map(({Label, type}) => {
           return (
           <div className="input-wrapper">
             <label className="form-label"> {Label}
             {type === 'textarea' ? (
-              <textarea className="input" name={Label} rows={10}></textarea>
+              <textarea className="input" onChange={(e) => setFormState({...formState, [Label]: e.target.value})} name={Label} rows={10}></textarea>
             ) : (
-              <input className="input" name={Label} type={type} required/>
+              <input className="input" onChange={(e) => setFormState({...formState, [Label]: e.target.value})} name={Label} type={type} required/>
             )}
               </label>
           </div> 
           )
         })}
         <input type="hidden" name="form-name" value="contact" />
-        <button className="send-button">
+        <button onClick={(e) => handleSubmit(e)}className="send-button">
           Send
           </button>
       </form>

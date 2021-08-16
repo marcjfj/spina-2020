f1820a28a53e979cc1c73f98d7033ea4a6f2dfb3
Marc <marcjfj@gmail.com>
2021-08-15 20:45:44 -0500

    adding form

--- src/components/blocks/Form.tsx
+++ src/components/blocks/Form.tsx
@@ -2,8 +2,24 @@ export default function Form({inputs}) {
   console.log(inputs)
   return (
     <section className="form-wrapper">
-      <form action="" className="form"></form>
-
+      <form action="POST" data-netlify="true" name="contact" className="form">
+        {inputs.map(({Label, type}) => {
+          return (
+          <div className="input-wrapper">
+            <label className="form-label"> {Label}
+            {type === 'textarea' ? (
+              <textarea className="input" rows={10}></textarea>
+            ) : (
+              <input className="input" type={type} required/>
+            )}
+              </label>
+          </div> 
+          )
+        })}
+        <button className="send-button">
+          Send
+          </button>
+      </form>
     </section>
   )
 }

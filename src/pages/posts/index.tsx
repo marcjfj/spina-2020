d8fff703419fb3b002e67b38434fec7776b4bed2
Marc <marcjfj@gmail.com>
2021-08-16 20:11:57 -0500

    homepage stuff

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -23,17 +23,5 @@ export default function Index(props) {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const posts = listPostContent(1, config.posts_per_page);
-  const tags = listTags();
-  const pagination = {
-    current: 1,
-    pages: Math.ceil(countPosts() / config.posts_per_page),
-  };
-  return {
-    props: {
-      posts,
-      tags,
-      pagination,
-    },
-  };
+  return null;
 };

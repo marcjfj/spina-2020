71b1f2b107a66346f7306af770a093b7fd66e372
Marc <marcjfj@gmail.com>
2021-08-16 21:16:39 -0500

    content fix

--- src/components/blocks/ContentImage.tsx
+++ src/components/blocks/ContentImage.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
 import hydrate from "next-mdx-remote/hydrate";
 
-export default function ContentImage({ title, image, content }) {
+export default function ContentImage({ title, image, content, layout }) {
   const parsedContent = hydrate(content);
   return (
-    <section className="content-image">
+    <section className={`content-image ${layout}`}>
       <div className="content">
         <h3 className="title">{title}</h3>
         {parsedContent}

fd2e6fe93d7fd25d1cab76c8b4c6daf913766150
Marc <marcjfj@gmail.com>
2021-10-25 22:13:40 -0500

    fix calendar

--- public/admin/config/blocks/Calendar.js
+++ public/admin/config/blocks/Calendar.js
@@ -5,25 +5,10 @@ export default {
   allow_add: true,
   fields: [
     {
-      name: "days",
-      title: "Days",
-      widget: "list",
-      fields: [
-        {
-          label: "Date",
-          name: "Date",
-          widget: "datetime",
-          date_format: "dddd, MMMM Do",
-          time_format: false,
-          format: "dddd, MMMM Do",
-          picker_utc: false,
-        },
-        {
-          label: "Hours",
-          name: "hours",
-          widget: "string",
-        }
-      ]
+      label: "Type",
+      name: "type",
+      widget: "hidden",
+      default: "main",
     }
   ]
 }
--- public/admin/config/calendar.js
+++ public/admin/config/calendar.js
@@ -6,10 +6,25 @@ export default {
   file: "content/calendar.json",
   fields: [
     {
-      label: "Type",
-      name: "type",
-      widget: "hidden",
-      default: "main",
+      name: "days",
+      title: "Days",
+      widget: "list",
+      fields: [
+        {
+          label: "Date",
+          name: "Date",
+          widget: "datetime",
+          date_format: "dddd, MMMM Do",
+          time_format: false,
+          format: "dddd, MMMM Do",
+          picker_utc: false,
+        },
+        {
+          label: "Hours",
+          name: "hours",
+          widget: "string",
+        }
+      ]
     }
   ]
 }
